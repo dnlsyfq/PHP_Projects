1b636e718b97970072cce45385fd28001b75b5f7
Danial Jalaluddin <dan90@live.com.my>
2020-06-28 14:18:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<body>
+<!--Your code goes here-->
+<h1>Addition</h1>
+<form method="get" action="addition.php">
+First No. :
+<input type="number" name="first">
+<br>
+Second No. :
+<input type="number" name="second">
+<br>
+<button type="submit">Submit</button>
+</form>
+
+<h1>Division</h1>
+<form method="get" action="division.php">
+Dividen No. :
+<input type="number" name="dividen">
+<br>
+Divisor No. :
+<input type="number" name="divisor">
+<br>
+<button type="submit">Submit</button>
+</form>
+
+<a href="index.php">Reset</a>
+</body>
+</html>
